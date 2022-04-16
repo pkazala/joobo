6f6a237974c12e8df45ab35e334487ef0adeb347
Piotrek <piotr.kazala.2020@uni.strath.ac.uk>
2022-04-16 16:02:21 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#F87171',
+        'dark': '#BD827E',
+        'light': '#FFF3F1',
+        'darker-light': '#F3EED9',
+        'joobo-gray': '#6B7280'
+      }
+    },
+    fontFamily: {
+      logo: ['Roboto']
+    }
+  },
+  plugins: [require("daisyui")],
+}
