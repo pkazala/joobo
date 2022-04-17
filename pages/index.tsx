f84b695745d300c2f0030fb37da538920d19d433
Piotrek <piotr.kazala.2020@uni.strath.ac.uk>
2022-04-17 23:27:32 +0200

    Few changes to the header

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ const Home: NextPage = () => {
       <p className="text-center mt-10 text-2xl font-thin">Here you will find everything you need to know to find your perfect job</p>
       <p className="text-center mt-2 text-2xl font-thin">Working to improve job search for every developer</p>
       <h1 className="text-center mt-2 text-2xl">Coming soon...</h1>
+      <h1 className='text-center mt-8 text-7xl'>🚀</h1>
     </div>
   )
 }
