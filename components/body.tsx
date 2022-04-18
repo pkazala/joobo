73c283a0dba4bffdf696ba10d5b6434500848a7c
Piotrek <piotr.kazala.2020@uni.strath.ac.uk>
2022-04-18 02:00:05 +0200

    Added the design for the main body section

--- components/body.tsx
+++ components/body.tsx
@@ -3,7 +3,7 @@ import TopCities from './topCities'
 const body = () => {
     return (
         <div className="flex justify-center">
-            <div className="w-full max-w-[150rem] h-96 m-7 grid grid-rows-3 grid-flow-col gap-4 mt-10">
+            <div className="w-full max-w-[150rem] h-fit m-7 grid grid-rows-3 grid-flow-col gap-4 mt-10">
                 <div className="row-span-2 col-span-2 bg-light">
                   <TopCities></TopCities>  
                 </div>
--- components/topCities.tsx
+++ components/topCities.tsx
@@ -1,9 +1,39 @@
 const topCities = () => {
     return (
-        <div className="grid grid-cols-3">
-            <div>2</div>
-            <div>1</div>
-            <div>3</div>
+        <div>
+            <h1 className="col-span-3 text-center font-logo font-light text-4xl m-3">Miasta z najwyższymi zarobkami</h1>
+            <div className="grid grid-cols-3 justify-items-center mt-10 m-5 gap-5">
+                <div className="card w-96 bg-base-100 shadow-xl text-center">
+                    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=300" alt="Shoes" /></figure>
+                    <div className="card-body bg-[#FAFAFA]">
+                        <h2 className="font-logo text-3xl m-2">Kraków</h2>
+                        <p className="font-logo font-light text-3xl mb-2">16,321 zl/m</p>
+                        <div className="card-actions justify-center">
+                            <button className="btn bg-main text-white hover:bg-white hover:text-black">Sprawdź</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl text-center">
+                    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=300" alt="Shoes" /></figure>
+                    <div className="card-body bg-[#FAFAFA]">
+                        <h2 className="font-logo text-3xl m-2">Kraków</h2>
+                        <p className="font-logo font-light text-3xl mb-2">16,321 zl/m</p>
+                        <div className="card-actions justify-center">
+                            <button className="btn bg-main text-white hover:bg-white hover:text-black">Sprawdź</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl text-center">
+                    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=300" alt="Shoes" /></figure>
+                    <div className="card-body bg-[#FAFAFA]">
+                        <h2 className="font-logo text-3xl m-2">Kraków</h2>
+                        <p className="font-logo font-light text-3xl mb-2">16,321 zl/m</p>
+                        <div className="card-actions justify-center">
+                            <button className="btn bg-main text-white hover:bg-white hover:text-black">Sprawdź</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
